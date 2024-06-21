7309cb4ac2b8c6b84d78ff49a8b19094b17c1c31
tamjid-ahmed <tamjidahmed644@gmail.com>
2024-06-21 19:27:38 +0600

    changes

--- app/messages/[id]/inbox.js
+++ app/messages/[id]/inbox.js
@@ -736,10 +736,10 @@ const peerc = new peers({
     'iceServers': [
 
  
-    { urls: 'stun:stun.l.google.com:19302' },
+    { urls: 'stun:stun1.l.google.com:3478' },
 
     {
-      urls : 'relay1.expressturn.com:3478',
+      urls : 'turn:relay1.expressturn.com:3478',
       username: 'efSTZSFZANGQ0JRJKJ',
       credential: 'YLDsoxNdRU4cnmOm'
 
